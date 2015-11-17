dabac7e226dcb264beb5c3b8681bd36b2eee22c2
Daniel Cook <daniel@danielcook.me>
2015-11-17 10:02:41 -0330

    Switched to simplified ChIrrApp methods - added missing 'DrawAll'

--- src/Main.cpp
+++ src/Main.cpp
@@ -29,17 +29,18 @@ int main(int argc, char* argv[])
     irr::ChIrrAppInterface app(&system, L"Terrain Leveling", irr::core::dimension2d<irr::u32>(800,600), false, true);
     
     //Convienience methods for scene setup
-    irr::ChIrrWizard::add_typical_Logo(app.GetDevice());
-    irr::ChIrrWizard::add_typical_Sky(app.GetDevice());
-    irr::ChIrrWizard::add_typical_Lights(app.GetDevice());
-    irr::ChIrrWizard::add_typical_Camera(app.GetDevice(), irr::core::vector3df(0, 0, -6), irr::core::vector3df(-2, 2, 0));
+    app.AddTypicalLogo();
+    app.AddTypicalCamera();
+    app.AddTypicalSky();
+    app.AddTypicalLights();
     
     app.SetStepManage(true);
     app.SetTimestep(dt);
     app.SetTryRealtime(true);    
     
     while(app.GetDevice()->run()){
-        app.GetVideoDriver()->beginScene(true, true, irr::video::SColor(255, 140, 161, 192));
+        app.BeginScene();
+        app.DrawAll();
         
         irr::ChIrrTools::drawGrid(app.GetVideoDriver(), 2, 2, 30, 30,
                                   ChCoordsys<>(ChVector<>(0, 0.01, 0), Q_from_AngX(CH_C_PI_2)),
@@ -47,7 +48,7 @@ int main(int argc, char* argv[])
                              
         app.DoStep();
         
-        app.GetVideoDriver()->endScene();
+        app.EndScene();
     }
     
     return 0;
