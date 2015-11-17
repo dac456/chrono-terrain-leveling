f3424c74875fc9f1c624a64a28f7ec2d1b697580
Daniel Cook <daniel@danielcook.me>
2015-11-16 23:16:01 -0330

    read timestep as --dt arg in main()

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 #include <irrlicht.h>
 
 #include <chrono/physics/ChSystem.h>
@@ -6,8 +8,23 @@
 //Usually I'm against this, but Chrono classes are all prefixed with 'Ch' so name collision isn't likely
 using namespace chrono;
 
+double dt = 0.3; //Default timestep
+
+void readArgs(int argc, char* argv[]){
+    int current = 1;
+    if(argc > 1){
+        if(strcmp(argv[current],"--dt") == 0){
+            if(argv[current+1] != nullptr){
+                dt = atof(argv[current+1]);
+            }
+        }
+    }
+}
+
 int main(int argc, char* argv[])
 {
+    readArgs(argc, argv);
+    
     ChSystem system;
     irr::ChIrrAppInterface app(&system, L"Terrain Leveling", irr::core::dimension2d<irr::u32>(800,600), false, true);
     
@@ -18,7 +35,7 @@ int main(int argc, char* argv[])
     irr::ChIrrWizard::add_typical_Camera(app.GetDevice(), irr::core::vector3df(0, 0, -6), irr::core::vector3df(-2, 2, 0));
     
     app.SetStepManage(true);
-    app.SetTimestep(0.03);
+    app.SetTimestep(dt);
     app.SetTryRealtime(true);    
     
     while(app.GetDevice()->run()){
