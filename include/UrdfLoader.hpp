7d7c5ada4e1cbab852a3b6191ad300a7c632d80d
Daniel Cook <daniel@danielcook.me>
2015-12-03 17:51:14 -0330

    Renamed urdf struct vectors - added child links

--- include/UrdfLoader.hpp
+++ include/UrdfLoader.hpp
@@ -12,24 +12,25 @@ struct UrdfVisual{
 
 struct UrdfLink{
     std::string name;
-    std::vector<UrdfVisual> visual;    
+    std::vector<UrdfVisual> visuals;
+    std::vector<UrdfLink> links;
 };
 
 class UrdfLoader{
 private:
     std::string _file;
-    
+
     std::vector<UrdfLink> _links;
 
 public:
     UrdfLoader(std::string file);
     ~UrdfLoader();
-    
+
 private:
     void _load();
     void _loadNode(rapidxml::xml_node<>* node);
     void _loadLink(rapidxml::xml_node<>* node, UrdfLink* link);
-    
+
     void _loadVisual(rapidxml::xml_node<>* node, UrdfLink* link);
 
 };
--- src/UrdfLoader.cpp
+++ src/UrdfLoader.cpp
@@ -5,23 +5,23 @@ UrdfLoader::UrdfLoader(std::string file)
 {
     //TODO:check if file exists
     _load();
-    
+
 }
 
 UrdfLoader::~UrdfLoader(){
-    
+
 }
 
 void UrdfLoader::_load(){
     std::string buffer;
-    
+
     std::ifstream fin(_file, std::ios::in);
     fin >> buffer;
     fin.close();
-    
+
     rapidxml::xml_document<> doc;
     doc.parse<0>(const_cast<char*>(buffer.c_str()));
-    
+
     if(doc.first_node()->name() == "robot"){
         _loadNode(doc.first_node());
     }
@@ -29,7 +29,7 @@ void UrdfLoader::_load(){
 
 void UrdfLoader::_loadNode(rapidxml::xml_node<>* node){
     rapidxml::xml_node<>* current = node;
-    
+
     while(current){
         if(current->name() == "robot"){
             current = current->first_node();
@@ -37,46 +37,53 @@ void UrdfLoader::_loadNode(rapidxml::xml_node<>* node){
         else if(current->name() == "link"){
             UrdfLink link;
             link.name = current->first_attribute("name")->value();
-            
+
             _loadLink(current, &link);
             _links.push_back(link);
         }
-        
+
         current = current->next_sibling();
     }
 }
 
 void UrdfLoader::_loadLink(rapidxml::xml_node<>* node, UrdfLink* link){
     rapidxml::xml_node<>* current = node->first_node();
-    
+
     while(current){
-        if(current->name() == "visual"){
+        if(current->name() == "link"){
+            UrdfLink child;
+            child.name = current->first_attribute("name")->value();
+
+            _loadLink(current, &child);
+            link->links.push_back(child);
+        }
+        else if(current->name() == "visual"){
             _loadVisual(current, link);
         }
         else if(current->name() == "collision"){
-            
+
         }
-        
+
         current = current->next_sibling();
     }
 }
 
 void UrdfLoader::_loadVisual(rapidxml::xml_node<>* node, UrdfLink* link){
     rapidxml::xml_node<>* current = node;
-    
+
     UrdfVisual visual;
     if(current->first_attribute("name")){
         visual.name = current->first_attribute("name")->value();
     }
-    
+
     current = current->first_node();
     while(current){
         if(current->name() == "geometry"){
-            
+
         }
-        
+
         current = current->next_sibling();
     }
-    
-    link->visual.push_back(visual);
+
+    link->visuals.push_back(visual);
 }
