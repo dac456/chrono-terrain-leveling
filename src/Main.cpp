c32e1df0ca156905e163ccf15d9c5058518c74d2
Daniel Cook <daniel@danielcook.me>
2016-01-04 00:06:49 -0330

    Added offline rendering path in Main based on Chrono demo_postprocess.

--- src/Main.cpp
+++ src/Main.cpp
@@ -4,27 +4,37 @@
 #include "TrackedVehicle.hpp"
 #include "ParticleSystem.hpp"
 
+#include <chrono/core/ChFileutils.h>
+#include <chrono_postprocess/ChPovRay.h>
+#include <chrono_postprocess/ChPovRayAssetCustom.h>
+using namespace postprocess;
+
 double dt = 0.01; //Default timestep
 size_t numThreads = 8; //Default thread count
+
+bool renderOffline = false;
 
 void readArgs(int argc, char* argv[]){
     int current = 1;
     if(argc > 1){
-        while(current < argc){
+        while(current < argc && argv[current] != nullptr){
             if(strcmp(argv[current],"--dt") == 0){
                 if(argv[current+1] != nullptr){
                     dt = atof(argv[current+1]);
                     current++;
                 }
-                current++;
             }
             if(strcmp(argv[current],"--nt") == 0){
                 if(argv[current+1] != nullptr){
                     numThreads = atoi(argv[current+1]);
                     current++;
                 }
-                current++;
             }
+            if(strcmp(argv[current], "--offline") == 0){
+                renderOffline = true;
+            }
+
+            current++;
         }
     }
 }
@@ -54,23 +64,11 @@ int main(int argc, char* argv[])
         system.GetSettings()->collision.bins_per_axis = I3(10, 10, 10);
     #endif
 
-    irr::ChIrrApp app(&system, L"Terrain Leveling", irr::core::dimension2d<irr::u32>(800,600), false, true);
-
-    //Convienience methods for scene setup
-    app.AddTypicalLogo();
-    app.AddTypicalCamera();
-    app.AddTypicalSky();
-    app.AddTypicalLights();
-
     system.SetIterLCPmaxItersSpeed(100);  // the higher, the easier to keep the constraints 'mounted'.
     system.SetLcpSolverType(ChSystem::LCP_ITERATIVE_SOR);
     //system.SetIterLCPmaxItersSpeed(70);
     //system.SetIterLCPmaxItersStab(15);
     system.SetParallelThreadNumber(numThreads);
-
-    app.SetStepManage(true);
-    app.SetTimestep(dt);
-    app.SetTryRealtime(true);
 
     //Default material
     ChMaterialPtr mat(new ChMaterialSurface);
@@ -82,26 +80,82 @@ int main(int argc, char* argv[])
 
 
     UrdfLoader urdf(GetChronoDataFile("urdf/Dagu5.urdf"));
-    AssemblyPtr testAsm = std::make_shared<Assembly>(urdf, ChVectord(0,4,0), static_cast<ChSystem*>(&system));
+    AssemblyPtr testAsm = std::make_shared<Assembly>(urdf, ChVectord(0,3,0), static_cast<ChSystem*>(&system));
 
     TrackedVehiclePtr dagu = std::make_shared<TrackedVehicle>("dagu001", "shoe_view.obj", "shoe_collision.obj", testAsm, 0.5);
 
     ParticleSystemPtr particles = std::make_shared<ParticleSystem>(static_cast<ChSystem*>(&system), ChVectord(20,2,20), 100.0, 0.15);
 
-    app.AssetBindAll();
-    app.AssetUpdateAll();
+    if(renderOffline == false){
+        irr::ChIrrApp app(&system, L"Terrain Leveling", irr::core::dimension2d<irr::u32>(800,600), false, true);
 
-    while(app.GetDevice()->run()){
-        app.BeginScene();
-        app.DrawAll();
+        app.SetStepManage(true);
+        app.SetTimestep(dt);
+        app.SetTryRealtime(true);
 
-        irr::ChIrrTools::drawGrid(app.GetVideoDriver(), 2, 2, 30, 30,
-                                  ChCoordsys<>(ChVector<>(0, 0.01, 0), Q_from_AngX(CH_C_PI_2)),
-                                  irr::video::SColor(255, 60, 60, 60), true);
+        //Convienience methods for scene setup
+        app.AddTypicalLogo();
+        app.AddTypicalCamera(irr::core::vector3df(8,8,8));
+        app.AddTypicalSky();
+        app.AddTypicalLights();
 
-        app.DoStep();
+        app.AssetBindAll();
+        app.AssetUpdateAll();
 
-        app.EndScene();
+        while(app.GetDevice()->run()){
+            app.BeginScene();
+            app.DrawAll();
+
+            irr::ChIrrTools::drawGrid(app.GetVideoDriver(), 2, 2, 30, 30,
+                                      ChCoordsys<>(ChVector<>(0, 0.01, 0), Q_from_AngX(CH_C_PI_2)),
+                                      irr::video::SColor(255, 60, 60, 60), true);
+
+            app.DoStep();
+
+            app.EndScene();
+        }
+    }
+    else{
+        ChPovRay app = ChPovRay(&system);
+
+        app.SetTemplateFile(GetChronoDataFile("_template_POV.pov"));
+        app.SetOutputScriptFile("rendering_frames.pov");
+        app.SetOutputDataFilebase("my_state");
+        app.SetPictureFilebase("picture");
+
+        ChFileutils::MakeDirectory("output");
+        ChFileutils::MakeDirectory("anim");
+
+        app.SetOutputDataFilebase("output/my_state");
+        app.SetPictureFilebase("anim/picture");
+
+        app.SetLight(ChVector<>(-3, 4, 2), ChColor(0.15f, 0.15f, 0.12f), false);
+        app.SetCamera(ChVectord(8,8,8), ChVectord(0,0,0), 50.0);
+
+        // --Optional: add further POV commands, for example in this case:
+        //     create an area light for soft shadows
+        app.SetCustomPOVcommandsScript(
+            " \
+            light_source {   \
+                <2, 10, -3>  \
+                color rgb<1.2,1.2,1.2> \
+                area_light <4, 0, 0>, <0, 0, 4>, 8, 8 \
+                adaptive 1 \
+                jitter\
+            } \
+        ");
+
+        app.AddAll();
+
+        app.ExportScript();
+
+        while(1) {
+            system.DoStepDynamics(dt);
+
+            std::cout << "time= " << system.GetChTime() << std::endl;
+
+            app.ExportData();
+        }
     }
 
     return 0;
