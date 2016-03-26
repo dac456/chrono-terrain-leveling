22bd537f7336eb855096f3115702c8b646f9271a
Daniel Cook <daniel@danielcook.me>
2016-03-26 13:33:22 -0230

    - Fixed chrono data path in Experiment

--- src/Experiment.cpp
+++ src/Experiment.cpp
@@ -21,7 +21,8 @@ Experiment::Experiment(ChSystem* system, std::string expConfigFile)
     _vm = cfg.getVariables();
 
     fs::create_directories(_vm["output_directory_prefix"].as<std::string>() + "framedata/");
-    SetChronoDataPath(fs::canonical(_vm["chrono_data_path"].as<std::string>() + '/').string());
+    std::cout << fs::canonical(_vm["chrono_data_path"].as<std::string>()).string() << std::endl;
+    SetChronoDataPath(fs::canonical(_vm["chrono_data_path"].as<std::string>() + '/').string() + '/');
 
     _name = _vm["experiment.name"].as<std::string>();
 
