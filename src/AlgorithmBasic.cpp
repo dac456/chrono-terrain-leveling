06ee5c909d8b42b0ece3a8ac6aae51e5ab7e95f9
Daniel Cook <daniel@danielcook.me>
2016-04-01 03:01:47 -0230

    - Updated AlgorithmBasic

--- src/AlgorithmBasic.cpp
+++ src/AlgorithmBasic.cpp
@@ -16,17 +16,18 @@ void AlgorithmBasic::senseImpl(float dt) {
 }
 
 void AlgorithmBasic::actImpl(float dt) {
-    /*if(isInclined()){
+    if(isInclined()){
         setDesiredLinearVelocity(3.0f);
         setDesiredAngularVelocity(0.0f);
     }
     else{
         setDesiredLinearVelocity(2.0f);
-        if(ChRandom() < 0.25) setDesiredAngularVelocity(ChRandom() * 1.57);
+        
+        double r = ChRandom();
+        if(r < 0.25) setDesiredAngularVelocity(1.57);
+        else if(r >= 0.25 && r < 0.5) setDesiredAngularVelocity(-1.57);
         else setDesiredAngularVelocity(0.0f);
-    }*/
-    setDesiredLinearVelocity(4.0f);
-    setDesiredAngularVelocity(0.0f);
+    }
 
     move();
 }
